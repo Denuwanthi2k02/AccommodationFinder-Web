fd2a54b07f10d36373d4014d230c81bbceadadff
Dewmini Denuwanthi <denuwanthidewmini@gmail.com>
2025-11-11 01:06:11 +0530

    Add Docker setup for project

--- server/router/ownerRouter.js
+++ server/router/ownerRouter.js
@@ -1,11 +1,58 @@
+// import express from "express";
+// import { protect } from "../middleware/auth.js";
+// import { addAnnex, changeRoleToOwner, deleteAnnex, getOwnerAnnex, toggleAnnexAvailability,getDashboardData, updateUserImage ,updateAnnex,updateUserProfile} from "../controllers/ownerController.js";
+// import upload from "../middleware/multer.js"
+
+// const ownerRouter = express.Router();
+
+// ownerRouter.post("/change-role", protect, changeRoleToOwner);
+// ownerRouter.post(
+//   "/add-accommodation",
+//   upload.fields([
+//     { name: "image", maxCount: 1 },
+//     { name: "gallery", maxCount: 4 },
+//   ]),
+//   protect,
+//   addAnnex
+// );
+// ownerRouter.get("/accommodations" ,protect,getOwnerAnnex)
+// ownerRouter.post("/toggle-accommodation" ,protect,toggleAnnexAvailability)
+// ownerRouter.post("/delete-accommodation" ,protect,deleteAnnex)
+// ownerRouter.get("/dashboard" ,protect,getDashboardData)
+// ownerRouter.post("/update-image" ,protect,upload.single("image"),updateUserImage)
+// ownerRouter.put("/update-profile", protect, updateUserProfile);
+// ownerRouter.put(
+//   "/update-accommodation",
+//   upload.fields([
+//     { name: "image", maxCount: 1 },
+//     { name: "gallery", maxCount: 4 },
+//   ]),
+//   protect,
+//   updateAnnex
+// );
+// export default ownerRouter;
+
+
+
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { addAnnex, changeRoleToOwner, deleteAnnex, getOwnerAnnex, toggleAnnexAvailability,getDashboardData, updateUserImage ,updateAnnex,updateUserProfile} from "../controllers/ownerController.js";
-import upload from "../middleware/multer.js"
+import {
+  addAnnex,
+  changeRoleToOwner,
+  deleteAnnex,
+  getOwnerAnnex,
+  toggleAnnexAvailability,
+  getDashboardData,
+  updateUserImage,
+  updateAnnex,
+  updateUserProfile,
+} from "../controllers/ownerController.js";
+import upload from "../middleware/multer.js";
 
 const ownerRouter = express.Router();
 
 ownerRouter.post("/change-role", protect, changeRoleToOwner);
+
 ownerRouter.post(
   "/add-accommodation",
   upload.fields([
@@ -15,12 +62,14 @@ ownerRouter.post(
   protect,
   addAnnex
 );
-ownerRouter.get("/accommodations" ,protect,getOwnerAnnex)
-ownerRouter.post("/toggle-accommodation" ,protect,toggleAnnexAvailability)
-ownerRouter.post("/delete-accommodation" ,protect,deleteAnnex)
-ownerRouter.get("/dashboard" ,protect,getDashboardData)
-ownerRouter.post("/update-image" ,protect,upload.single("image"),updateUserImage)
+
+ownerRouter.get("/accommodations", protect, getOwnerAnnex);
+ownerRouter.post("/toggle-accommodation", protect, toggleAnnexAvailability);
+ownerRouter.post("/delete-accommodation", protect, deleteAnnex);
+ownerRouter.get("/dashboard", protect, getDashboardData);
+ownerRouter.post("/update-image", protect, upload.single("image"), updateUserImage);
 ownerRouter.put("/update-profile", protect, updateUserProfile);
+
 ownerRouter.put(
   "/update-accommodation",
   upload.fields([
@@ -30,4 +79,5 @@ ownerRouter.put(
   protect,
   updateAnnex
 );
+
 export default ownerRouter;
