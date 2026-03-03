03f3e89e487160599b80e20712800f981347b130
Dewmini Denuwanthi <denuwanthidewmini@gmail.com>
2026-03-03 19:31:39 +0530

    Update the title of the web

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
       {/* Left Content */}
       <div className="hero-text">
         <h1>
-          Welcome To <br /> <span>Denuwanthi</span>
+          Welcome To <br /> <span>AnnexMate</span>
         </h1>
         <p>
           Find your perfect stay – quick and easy! AnnexMate is designed for
